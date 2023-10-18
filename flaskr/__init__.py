ee7baeb6098ad40803c78bcbba31922612f33153
Nwani Ugonna Stanley <ugonna.nwani@gmail.com>
2023-10-19 00:01:22 +0100

    A1

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,7 +1,9 @@
 import os
+import openai
 from flask import Flask, request, abort, jsonify
 from flask_cors import CORS
 
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def create_app(test_config=None):
     # create and configure the app
@@ -38,6 +40,24 @@ def create_app(test_config=None):
                 )
 
             # TODO: Pass the text variable to the model and return the summary
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {
+                    "role": "system",
+                    "content": "You are a helpful assistant that summarizes text."
+                    },
+                    {
+                    "role": "user",
+                    "content": text
+                    }
+                    ],
+                temperature=1,
+                max_tokens=300,
+                top_p=1,
+                frequency_penalty=2,
+                presence_penalty=2
+                )
 
             return jsonify(
                 {
@@ -45,7 +65,7 @@ def create_app(test_config=None):
                     "message": "Summary generated successfully.",
                     "data": {
                         # TODO: Return the summary
-                        "summary": text,
+                        "summary": response['choices'][0]['message']['content'],
                     },
                 }
             )
@@ -60,18 +80,38 @@ def create_app(test_config=None):
             text = body.get("text", None)
             query = body.get("query", None)
 
-            return (
-                jsonify(
-                    {
-                        "success": False,
-                        "status": 422,
-                        "message": "Unprocessable. Please provide a text and a query.",
-                    }
-                ),
-                422,
-            )
+            if not text or not query:
+                return (
+                    jsonify(
+                        {
+                            "success": False,
+                            "status": 422,
+                            "message": "Unprocessable. Please provide a text and a query.",
+                        }
+                    ),
+                    422,
+                )
 
             # TODO: Pass the text and query variables to the model and return the answer
+            prompt = f"You are a helpful assistant that answers questions based on the words from the text below.\nText: \"\"\"\n{text}\n\"\"\""
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {
+                    "role": "system",
+                    "content": prompt
+                    },
+                    {
+                    "role": "user",
+                    "content": query
+                    }
+                ],
+                temperature=1,
+                max_tokens=256,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0
+                )
 
             return jsonify(
                 {
@@ -79,7 +119,7 @@ def create_app(test_config=None):
                     "message": "Text queried successfully.",
                     "data": {
                         # TODO: Return the answer to the query
-                        "reply": text,
+                        "reply": response['choices'][0]['message']['content'],
                     },
                 }
             )
